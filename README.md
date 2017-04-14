8cf7f4ac9cddcb27de2df7fa94d89d09c624d7a3
DonPandon <mightypanda@gmail.com>
2017-04-13 20:36:12 -0400

    A ver si ahora si se actualiza

--- README.md
+++
@@ -1,3 +0,0 @@
-# C_Sharp_GitHub
-Primer Upload con nuevo Repositorio
-Este debe ser el bueno para corroborar trabajo en C#
---
+++ Serial_Comm_app/SerialCommTesterTerminal/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;      //to be able to use serial ports
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static Handshake SetPortHandshake(Handshake defaultPortHandshake)
+        {
+            string handshake;
+
+            Console.WriteLine("Available Handshake options:");
+            foreach (string s in Enum.GetNames(typeof(Handshake)))
+            {
+                Console.WriteLine("   {0}", s);
+            }
+
+            Console.Write("Enter Handshake value (Default: {0}):", defaultPortHandshake.ToString());
+            handshake = Console.ReadLine();
+
+            if (handshake == "")
+            {
+                handshake = defaultPortHandshake.ToString();
+            }
+
+            return (Handshake)Enum.Parse(typeof(Handshake), handshake, true);
+        }
+
+    }
+}
